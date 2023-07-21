d37b6675a40629fc701ccc4bce1441540783c1a8
YouCanCallMeRoot <140123417+RootGeek@users.noreply.github.com>
2023-07-21 19:19:28 +0200

    Create encoder.py

---
+++ encoder.py
@@ -0,0 +1,25 @@
+import requests
+
+# Deine gewünschte ASCII Art
+ascii_art = """
+ ____    ___    ___   _____   ____  _____  _____  _  __
+|  _ \  / _ \  / _ \ |_   _| / ___|| ____|| ____|| |/ /
+| |_) || | | || | | |  | |  | |  _ |  _|  |  _|  | ' / 
+|  _ < | |_| || |_| |  | |  | |_| || |___ | |___ | . \ 
+|_| \_\ \___/  \___/   |_|   \____||_____||_____||_|\_\
+"""
+
+def url_decoder(url):
+    try:
+        response = requests.head(url, allow_redirects=True)
+        decoded_url = response.url
+        return decoded_url
+    except requests.exceptions.RequestException:
+        return "Entschlüsselung fehlgeschlagen: Fehler bei der Verbindung zum Shortener-Dienst."
+
+if __name__ == "__main__":
+    print(ascii_art)
+    short_url = "http://bitly.ws/?redirect=M56e"  # Beispiel für den Zwischen-URL
+
+    decoded_url = url_decoder(short_url)
+    print("Entschlüsselter Original-Link:", decoded_url)
